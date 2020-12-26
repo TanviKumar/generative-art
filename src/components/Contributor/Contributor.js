55a82365fe46a5fdae294bcdcbde6243baa360f3
Purnima Sharma <57852378+purnima143@users.noreply.github.com>
2020-12-27 00:30:51 +0530

    Added sidebar

--- src/components/Contributor/Contributor.js
+++ src/components/Contributor/Contributor.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Contributor.css'
-import '../../App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -11,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
       flexGrow: 1,
     },
     paper: {
-      padding: theme.spacing(2),
+      margin: theme.spacing(5),
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
@@ -22,29 +21,53 @@ export default function Contributor() {
 
   return (
     <div className={classes.root}>
-    <Container fixed>
 
+    <Container fixed >
     <h1>Contributor Page</h1>
-      <Grid container spacing={3}>
+
+    {/* Row1 of the page*/}
+      <Grid container spacing={7} className="rows-contributor">
         
-        <Grid item md={3} xs={12}>
+        <Grid item md={4} xs={12}>
           <Paper className={classes.paper}>
-              <div className="circle">
-              
-                  <img src="https://images.unsplash.com/photo-1608652763693-376f0ce46283?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" altt=""/>
-              </div>
           </Paper>
         </Grid>
-        <Grid item md={3} xs={12}>
-          <Paper className={classes.paper}>md=3</Paper>
+        <Grid item md={4} xs={12}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item md={3} xs={12}>
-          <Paper className={classes.paper}>md=3</Paper>
+        <Grid item md={4} xs={12}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item md={3} xs={12}>
-          <Paper className={classes.paper}>xs=3</Paper>
+      
+      </Grid>
+      
+      {/* Row2 of the page*/}
+
+
+      <Grid container spacing={7} className="rows-contributor">
+        
+        <Grid item md={4} xs={12}>
+          <Paper className={classes.paper}>
+          </Paper>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <Paper className={classes.paper}>
+          <img src="https://images.unsplash.com/photo-1608652763693-376f0ce46283?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" altt=""/>
+
+          </Paper>
         </Grid>
       </Grid>
+
+
+
+
+
+
       </Container>
     </div>
   );
