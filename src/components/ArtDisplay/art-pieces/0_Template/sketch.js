1dbd0d3659049b732ef683afb1be24250c944f64
Rishabh Taparia <rishabh.taparia5@gmail.com>
2021-01-18 16:59:43 +0530

    Feat:Added Filtration component to the dashboard

--- src/components/ArtDisplay/art-pieces/0_Template/sketch.js
+++ src/components/ArtDisplay/art-pieces/0_Template/sketch.js
@@ -1,73 +1,72 @@
-import * as dat from 'dat.gui'; //Importing the GUI Library
-export default function sketch(p){
-/*================================================================ */
-/*Default Values for Grid size (can be updated by the user) */
-/*================================================================ */
-    // Padding around the canvas.
-    let padding = 15;
-    let gap = 20;
-    // Size of grid cells (cellSize x cellSize).
-    let cellSize = 50;
-    let gridSize;
-/*================================================================ */
-/*          Functions required for making the art-piece            */
-/*================================================================ */
-    // Setup function of p5.js.
-    // It runs once when the art-piece is loaded
-    p.setup = ()=>{
-        // uiRow - Number of squares per row/column
-        gridSize = cellSize * uiRow.getValue() + padding * 2;
-        p.createCanvas(gridSize, gridSize);
-        p.rectMode(p.CENTER);
-        p.angleMode(p.DEGREES);
-        p.createCanvas(gridSize, gridSize);
-        p.strokeWeight(2);
-        p.noLoop();
-        p.frameRate(2);
+import * as dat from "dat.gui"; //Importing the GUI Library
+export default function sketch(p) {
+  /*================================================================ */
+  /*Default Values for Grid size (can be updated by the user) */
+  /*================================================================ */
+  // Padding around the canvas.
+  let padding = 15;
+  let gap = 20;
+  // Size of grid cells (cellSize x cellSize).
+  let cellSize = 50;
+  let gridSize;
+  /*================================================================ */
+  /*          Functions required for making the art-piece            */
+  /*================================================================ */
+  // Setup function of p5.js.
+  // It runs once when the art-piece is loaded
+  p.setup = () => {
+    // uiRow - Number of squares per row/column
+    gridSize = cellSize * uiRow.getValue() + padding * 2;
+    p.createCanvas(gridSize, gridSize);
+    p.rectMode(p.CENTER);
+    p.angleMode(p.DEGREES);
+    p.createCanvas(gridSize, gridSize);
+    p.strokeWeight(2);
+    p.noLoop();
+    p.frameRate(2);
+  };
+  // draw() runs after setup(), normally on a loop
+  // in this case it runs only once, because of noLoop()
+  p.draw = () => {
+    p.background(255);
+    for (let y = padding; y < p.height - 2 * padding; y += cellSize + gap) {
+      for (let x = padding; x < p.width - 2 * padding; x += cellSize + gap) {
+        drawCell(x, y);
+      }
     }
-    // draw() runs after setup(), normally on a loop
-    // in this case it runs only once, because of noLoop()
-    p.draw = () => {
-        p.background(255);
-        for (let y = padding; y < p.height - 2 * padding; y += cellSize + gap) {
-          for (let x = padding; x < p.width - 2 * padding; x += cellSize + gap) {
-            drawCell(x, y);
-          }
-        }
-    };
+  };
 
-    function drawCell(x,y){
-        p.push();
-        p.translate(x, y);
-        p.noFill();
-        p.rect(0, 0, cellSize, cellSize);
-        p.pop();
+  function drawCell(x, y) {
+    p.push();
+    p.translate(x, y);
+    p.noFill();
+    p.rect(0, 0, cellSize, cellSize);
+    p.pop();
+  }
+  /*================================================================ */
+  /*                    GUI Specific Properties                      */
+  /*================================================================ */
+  // Create a class with the name of the art-piece
+  // Add the properties to be changed by GUI
+  // as class members
+  class Example {
+    constructor() {
+      this.Row = 12;
     }
-/*================================================================ */
-/*                    GUI Specific Properties                      */
-/*================================================================ */
-    // Create a class with the name of the art-piece
-    // Add the properties to be changed by GUI
-    // as class members
-    class Example{
-        constructor() {
-            this.Row = 12;
-        }
-    }
-    // Create a new class object
-    const example = new Example();
-    // Create gui object
-    const gui = new dat.GUI();
-    // Add the required properties to the GUI Controller
-    const uiRow = gui.add(example,"Row",5,20,1);
-    // On change of the property which functions to be called
-    uiRow.onChange(()=>{
-        p.setup();
-        p.draw();
-    });
-    gui.close();
-    window.onpopstate = function (e) {
-        gui.destroy();
-    };
-
+  }
+  // Create a new class object
+  const example = new Example();
+  // Create gui object
+  const gui = new dat.GUI();
+  // Add the required properties to the GUI Controller
+  const uiRow = gui.add(example, "Row", 5, 20, 1);
+  // On change of the property which functions to be called
+  uiRow.onChange(() => {
+    p.setup();
+    p.draw();
+  });
+  gui.close();
+  window.onpopstate = function (e) {
+    gui.destroy();
+  };
 }
--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -3,9 +3,11 @@ import "./DashBoard.css";
 import Card from "../Card/Card";
 import Grid from "@material-ui/core/Grid";
 import { Container } from "@material-ui/core";
+import FilterTags from "../FilterTags/FilterTags";
 import Axios from "axios";
 export default function DashBoard() {
   const [data, setData] = useState([]);
+  const [filterTag, setFilterTag] = useState("");
   useEffect(() => {
     Axios.get("./data.json")
       .then((res) => {
@@ -22,28 +24,61 @@ export default function DashBoard() {
     5: "Architecture",
     6: "Procedural Generation",
     7: "Noise",
-    8: "L-Systems"
-  }
+    8: "L-Systems",
+  };
   return (
     <div className="dashboard">
       <h1 className="title">Generative Art for All</h1>
-      <Container style={{ margin: "5% auto" }}>
-        <Grid container spacing={2}>
+      <FilterTags setFilterTag={setFilterTag} index={index}></FilterTags>
+      <Container style={{ margin: "5% auto", width: "100%" }}>
+        <Grid container spacing={1}>
           {data.map((content) => {
-            return (
-              <Grid item xl={4} l={4} xs={12} sm>
-                <Container style={{ margin: "40px 0" }}>
-                  <Card
-                    name={content.name}
-                    desc={content.desc}
-                    image={content.image}
-                    linkUrl={content.linkUrl}
-                    codeUrl={content.codeUrl}
-                    author={content.author}
-                  ></Card>
-                </Container>
-              </Grid>
-            );
+            const flag = content.tags.find((el) => {
+              return el === filterTag;
+            });
+            if (!filterTag) {
+              return (
+                <Grid item xl={4} l={4} xs={12} sm>
+                  <Container
+                    style={{
+                      marginTop: "40px",
+                      marginBottom: "40px",
+                      width: "max-content",
+                    }}
+                  >
+                    <Card
+                      name={content.name}
+                      desc={content.desc}
+                      image={content.image}
+                      linkUrl={content.linkUrl}
+                      codeUrl={content.codeUrl}
+                      author={content.author}
+                    ></Card>
+                  </Container>
+                </Grid>
+              );
+            } else if (flag !== undefined) {
+              return (
+                <Grid item xl={4} l={4} xs={12} sm>
+                  <Container
+                    style={{
+                      marginTop: "40px",
+                      marginBottom: "40px",
+                      width: "max-content",
+                    }}
+                  >
+                    <Card
+                      name={content.name}
+                      desc={content.desc}
+                      image={content.image}
+                      linkUrl={content.linkUrl}
+                      codeUrl={content.codeUrl}
+                      author={content.author}
+                    ></Card>
+                  </Container>
+                </Grid>
+              );
+            }
           })}
         </Grid>
       </Container>
