5619b225113e36f5bb78ec09598f0158a6ce7f37
Anurag <anuraggoyal.awr@gmail.com>
2021-08-24 21:40:04 +0530

    collatz recursive to iterative

--- src/components/ArtDisplay/art-pieces/Collatz/sketch.js
+++ src/components/ArtDisplay/art-pieces/Collatz/sketch.js
@@ -1,19 +1,31 @@
 import * as dat from "dat.gui";
 export default function sketch(p) {
+  let stackSizeLimit = 50000;
+  let centX = [5, 2.5, 3, 1.48];
+  let centY = [1.9, 2.1, 1.9, 1.65];
+  let compressRatios = [0.18e+7, 0.5e+7, 1.6e+7, 2.6e+7];
+
   p.setup = function () {
-    p.createCanvas(1300, 1100);
+    p.createCanvas(1000, 900);
     p.angleMode(p.DEGREES);
     p.background(uiBg.getValue());
-    p.strokeWeight(1.1);
+    p.strokeWeight(1);
+    p.noLoop();
   };
 
   p.draw = function () {
-    p.translate(p.width / 4, p.height / 1.9);
+    setCenter();
     p.push();
-    collatzRecursive(1, 0);
-    //collatzIterative();
+    collatzIterative();
     p.pop();
   };
+
+  function setCenter() {
+    let i = (uiLimit.getValue() / 10000) - 1;
+    let posX = p.width / centX[i];
+    let posY = p.height / centY[i];
+    p.translate(posX, posY);
+  }
 
   function setStroke(seed) {
     let red = uiRed.getValue() * p.noise(seed);
@@ -22,120 +34,69 @@ export default function sketch(p) {
     p.stroke(red, green, blue);
   }
 
-  function collatzRecursive(number, diff) {
-    if (number < 1) {
-      return;
-    }
-
-    if (number > uiLimit.getValue()) {
-      return;
-    }
-
-    let rot_angle = diff * 10;
-    p.rotate(rot_angle);
-    setStroke(diff);
-
-    let compressRatio = uiLimit.getValue() / 50;
-    diff = diff / compressRatio;
-    p.line(0, 0, 0, diff);
-    p.translate(0, diff);
-    p.push();
-
-    let nextNum = number * 2;
-    let nextDiff = number;
-    collatzRecursive(nextNum, nextDiff);
-
-    if (number % 3 === 1) {
-      nextNum = (number - 1) / 3;
-      nextDiff = -(number * 2 - 1) / 3;
-      collatzRecursive(nextNum, nextDiff);
-    }
-    
-    p.pop();
-  }
-
   function collatzIterative() {
-    let numStack = [{
+    let nodeStack = [{
       "num": 1,
       "diff": 0,
-      "index": 0,
+      "index": 0
     }];
-    let stateMap = new Map();
-    let visited = new Map();
-    let counter = 1;
+    let visitedSet = new Set();
+    let indexCounter = 1;
 
-    while (numStack.length > 0) {
-      let obj = numStack.pop();
-      let num = obj["num"];
-      let diff = obj["diff"];
-      let index = obj["index"];
-      let willProceed = false;
+    while ((nodeStack.length > 0) && (nodeStack.length <= stackSizeLimit)) {
+      let node = nodeStack.pop();
 
-      // pop any previously pushed states
-      for (let i = index + 1; i < counter; i++) {
-        if (typeof stateMap.get(i) == "boolean") {
-          p.pop();
-          stateMap.delete(i);
-        } else {
-          break;
-        }
+      if (visitedSet.has(node.index)) {
+        p.pop();
+        visitedSet.delete(node.index);
+        continue;
+      } else {
+        visitedSet.add(node.index);
       }
 
-      let rot_angle = diff * 10;
-      p.rotate(rot_angle);
-      setStroke(diff);
+      nodeStack.push(node);
 
-      let compressRatio = uiLimit.getValue() / 25;
-      diff = diff / compressRatio;
-      p.line(0, 0, 0, diff);
-      p.translate(0, diff);
+      let rot_angle = node.diff * 10;
+      p.rotate(rot_angle);
+      setStroke(node.diff);
+
+      let i = (uiLimit.getValue() / 10000) - 1;
+      let Ratio = uiLimit.getValue() / compressRatios[i];
+      let len = node.diff * Ratio;
+      p.line(0, 0, 0, len);
+      p.translate(0, len);
       p.push();
 
-      // work your way up the collatz tree
-      if (num * 2 < uiLimit.getValue()) {
-        let nextNum = num * 2;
-        if (typeof visited.get(nextNum) == "undefined") {
-          visited.set(nextNum, true);
-          let nextDiff = num;
-          numStack.push({
-            "num": nextNum,
-            "diff": nextDiff,
-            "index": counter
-          });
-          counter += 1;
-          willProceed = true;
-        }
+      let nextNum = 0;
+      let nextDiff = 0;
+
+      if ((node.num > 1) && (node.num % 3 === 1)) {
+        nextNum = (node.num - 1) / 3;
+        nextDiff = -((node.num * 2) + 1) / 3;
+        nodeStack.push({
+          "num": nextNum,
+          "diff": nextDiff,
+          "index": indexCounter
+        });
+        indexCounter ++;
       }
 
-      // work your way up the collatz tree
-      if (num % 3 === 1) {
-        let nextNum = (num - 1) / 3;
-        if (nextNum > 0) {
-          if (typeof visited.get(nextNum) == "undefined") {
-            visited.set(nextNum, true);
-            let nextDiff = -(num * 2 + 1) / 3;
-            numStack.push({
-              "num": nextNum,
-              "diff": nextDiff,
-              "index": counter
-            });
-            counter += 1;
-            willProceed = true;
-          }
-        }
-      }
-
-      if (willProceed) {
-        stateMap.set(index, true);
-      } else {
-        p.pop();
+      if (node.num * 2 <= uiLimit.getValue()) {
+        nextNum = node.num * 2;
+        nextDiff = node.num;
+        nodeStack.push({
+          "num": nextNum,
+          "diff": nextDiff,
+          "index": indexCounter
+        });
+        indexCounter ++;
       }
     }
   }
 
   class Collatz {
     constructor() {
-      this.Limit = 8000;
+      this.Limit = 1e+4;
       this.Red = 255;
       this.Green = 255;
       this.Blue = 255;
@@ -145,7 +106,7 @@ export default function sketch(p) {
 
   const collatz = new Collatz();
   const gui = new dat.GUI();
-  const uiLimit = gui.add(collatz, "Limit", 1000, 100000, 100);
+  const uiLimit = gui.add(collatz, "Limit", 1e+4, 4e+4, 1e+4);
   const uiRed = gui.add(collatz, "Red", 0, 255, 1);
   const uiGreen = gui.add(collatz, "Green", 0, 255, 1);
   const uiBlue = gui.add(collatz, "Blue", 0, 255, 1);
